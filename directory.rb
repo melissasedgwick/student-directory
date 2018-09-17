dd09f326bb3665218dee070556b5062f1fa96f9b
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-09-17 15:25:07 +0100

    Exercise 11 (correcting mistakes)

--- directory.rb
+++ directory.rb
@@ -1,83 +1,39 @@
 def input_students
-  @possible_cohorts = ["January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-]
-  puts "Please enter the name of the first student you'd like to enter"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
   students = []
-  name = gets
-  name.tr!("\n", "")
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    puts "Which cohort does #{name} belong to?"
-    cohort = gets.chomp.capitalize
-    while @possible_cohorts.include?(cohort) == false
-      puts "Please enter a correct cohort."
-      cohort = gets.chomp.capitalize
-    end
-    puts "What is #{name}'s hobby?"
-    hobby = gets.chomp
-    puts "What is #{name}'s height (in metres)?"
-    height = gets.chomp
-    puts "What is #{name}'s date of birth (in format DD/MM/YYYY)?"
-    dob = gets.chomp
-    students << {name: name,
-      cohort: cohort.to_sym,
-      hobby: hobby.to_sym,
-      height: height.to_sym,
-      DOB: dob.to_sym
-    }
-    if students.count == 1
-      puts "Now we have #{students.count} student"
-    else
-      puts "Now we have #{students.count} students"
-    end
-    puts "What is the next student's name?(If none, hit return)"
-    name = gets.chomp
+  # add the student hash to the array
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  # get another name from the user
+  name = gets.chomp
   end
+  # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(100)
-  puts "-------------".center(100)
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
+
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
-    puts "is #{student[:height]}m tall, was born #{student[:DOB]} and likes #{student[:hobby]}".center(100)
-  end
-end
-def print_by_cohort(students)
-  @possible_cohorts.each do |month|
-    if students.any? { |student| student[:cohort] == month.to_sym }
-      puts "#{month}:".center(100)
-    end
-    students.each do |student|
-      if student[:cohort] == month.to_sym
-        puts student[:name].center(100)
-      end
-    end
-  end
-end
-def print_footer(students)
-  puts "-------------".center(100)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student.".center(100)
-  else
-    puts "Overall, we have #{students.count} great students.".center(100)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+
 students = input_students
 print_header
-print_by_cohort(students)
+print(students)
 print_footer(students)
