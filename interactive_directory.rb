b0208143dd2e6c1770f4fe67f38d743d978397ac
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-09-18 09:51:34 +0100

    Added option to save list of students to .csv file

--- interactive_directory.rb
+++ interactive_directory.rb
@@ -1,5 +1,15 @@
 @students = []
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student [:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
   @possible_cohorts = ["January",
   "February",
@@ -83,6 +93,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -105,6 +116,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
